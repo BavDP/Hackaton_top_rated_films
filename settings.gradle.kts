deca409bae8c4283581b6c0f68709146079ced49
BavDP <alexander.beldii.ext@blablacar.com>
2024-02-15 02:17:12 +0200

    new hackaton project

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hackaton_15_02_24_top_rated_films"
+include(":app")
